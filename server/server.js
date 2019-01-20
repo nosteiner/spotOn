b2a29248bfec46105ee63abb01aeb5d547078417
nosteiner <no.steiner@gmail.com>
2019-01-20 18:31:22 +0200

    running db and server

--- server/server.js
+++ server/server.js
@@ -9,17 +9,20 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'dist/spoton')));
+app.use(express.static(path.join(__dirname, '../dist/spoton')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/spoton/index.html'));
-  });
-  
-  const port = process.env.PORT || '3000';
-  app.set('port', port);
-  
-  mongoose.connect(process.env.CONNECTION_STRING||'mongodb://localhost/spoton');
+});
 
-  const server = http.createServer(app);
+const port = process.env.PORT || '8080';
+app.set('port', port);
 
-  server.listen(port, () => console.log(`API running on localhost:${port}`));
+mongoose.connect(process.env.CONNECTION_STRING || 'mongodb://localhost/spoton', function () {
+    console.log("DB connected");
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port}`));
+
